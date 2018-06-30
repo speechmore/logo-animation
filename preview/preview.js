5991def0e9ed8adcbc40179e7538757f81bc44d5
Samuel Elliott <samuel@fancy.org.uk>
2018-06-30 20:59:36 +0100

    Add preview.js

---
+++ preview/preview.js
@@ -0,0 +1,10 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('sm'),
+    path: '../data.json',
+    loop: false,
+    renderer: 'svg',
+    autoplay: true,
+    name: "SpeechMore"
+});
+
+animation.setSpeed(0.9);
